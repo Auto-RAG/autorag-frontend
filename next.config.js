832f43669f1af63041312b117b1781f7a59e9506
홍승우 <martin@hongseung-uui-MacBookAir.local>
2024-11-16 22:45:49 +0900

    🚑 fix: Enable experimental features in webpack configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     distDir: '.next', // Replace 'build' with your desired directory name
+    webpack: (config, { isServer }) => {
+        config.experiments = {
+            ...config.experiments,
+            topLevelAwait: true,
+            layers: true,
+        };
+        return config;
+    },
 };
 
 module.exports = nextConfig;
