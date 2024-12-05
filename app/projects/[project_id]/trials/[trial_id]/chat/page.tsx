cb5007a821059bdf88d9b67285faf56543183f54
Jeffrey (Dongkyu) Kim <vkehfdl1@gmail.com>
2024-12-05 18:30:35 +0900

    Build correctly

--- app/projects/[project_id]/trials/[trial_id]/chat/page.tsx
+++ app/projects/[project_id]/trials/[trial_id]/chat/page.tsx
@@ -3,10 +3,11 @@ import { ChatPage } from "@/components/projects/trials/chat/chat-page";
 export default async function TrialChatPage({ params }: {params: Promise<{ project_id: string; trial_id: string }>;}) 
 {
   const { project_id, trial_id } = await params;
+  const hostUrl = process.env.NEXT_PUBLIC_HOST_URL;
 
   return (
     <div className="container mx-auto">
-      <ChatPage project_id={project_id} trial_id={trial_id} />
+      <ChatPage chat_url={`${hostUrl}:8501`} project_id={project_id} trial_id={trial_id}/>
     </div>
   );
 }
