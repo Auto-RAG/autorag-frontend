c9c356c1b8867a16c82cd2f3c5fd46b5ae6484e1
Jeffrey (Dongkyu) Kim <vkehfdl1@gmail.com>
2024-12-05 18:31:02 +0900

    Build correctly (#37)

--- app/projects/[project_id]/trials/[trial_id]/report/page.tsx
+++ app/projects/[project_id]/trials/[trial_id]/report/page.tsx
@@ -3,10 +3,11 @@ import { ReportPage } from "@/components/projects/trials/report/report-page";
 export default async function TrialReportPage({ params }: {params: Promise<{ project_id: string; trial_id: string }>;}) 
 {
   const { project_id, trial_id } = await params;
+  const hostUrl = process.env.NEXT_PUBLIC_HOST_URL;
 
   return (
     <div className="container mx-auto">
-      <ReportPage project_id={project_id} trial_id={trial_id} />
+      <ReportPage dashboard_url={`${hostUrl}:7690`} project_id={project_id} trial_id={trial_id}/>
     </div>
   );
 }
